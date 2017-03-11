92066bde16611c095701d0dc6443e03e9d958d5c
Mura, William <william.mura@nuance.com>
2017-03-10 22:51:14 -0500

    Update gitignore and add left menu

---
+++ components/common/left_menu.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+
+class Menu extends Component {
+    render() {
+        return (
+            <View>
+                <Text>
+                    Menu 1
+                </Text>
+                <Text>
+                    Menu 2
+                </Text>
+                <Text>
+                    Menu 3
+                </Text>
+                <Text>
+                    Menu 4
+                </Text>
+            </View>
+        );
+    }
+}
+
+module.exports = Menu;
