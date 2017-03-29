206c60b008bcf6e4c2d14566ee521681022772cd
Mura, William <william.mura@nuance.com>
2017-03-28 22:04:58 -0400

    Update Host api

--- components/common/constants.js
+++ components/common/constants.js
@@ -1,17 +1,19 @@
 var CONSTANTS = {
-    HOST_VM: 'http://206.167.181.119'
+    HOST_API: 'http://52.170.202.244:80',
+    HOST_PLATONIX: 'http://52.170.202.244:5010',
+    HOST_YEOUL: 'http://52.170.202.244:5011'
 };
 
 var URL = {
-    login:      CONSTANTS.HOST_VM + '/api/login',
-    register:   CONSTANTS.HOST_VM + '/api/users',
-    profile:    CONSTANTS.HOST_VM + '/api/users/me',
-    token:      CONSTANTS.HOST_VM + '/api/users/me/token',
-    signal:     CONSTANTS.HOST_VM + '/api/signal',
+    login:      CONSTANTS.HOST_API + '/api/login',
+    register:   CONSTANTS.HOST_API + '/api/users',
+    profile:    CONSTANTS.HOST_API + '/api/users/me',
+    token:      CONSTANTS.HOST_API + '/api/users/me/token',
+    signal:     CONSTANTS.HOST_API + '/api/signal',
 
-    photo:      CONSTANTS.HOST_VM + '/platonix/photo',
+    photo:      CONSTANTS.HOST_PLATONIX + '/platonix/photo',
 
-    parkings:   CONSTANTS.HOST_VM + '/yeoulparking/parkings'
+    parkings:   CONSTANTS.HOST_YEOUL + '/yeoulparking/parkings'
 };
 
 module.exports = {
